a71ac1503a7f9bc08ca3e5be9001a7e0e3e716da
Sunimal Malkaklulage <sskmal@gmail.com>
2024-06-02 15:38:43 +0900

    Config web security

--- src/main/java/com/loolcorp/alipathdemo/config/WebSecurityConfig.java
+++ src/main/java/com/loolcorp/alipathdemo/config/WebSecurityConfig.java
@@ -1,8 +1,55 @@
-package com.loolcorp.alipathdemo.config;/**
- * @Created 02/06/2024 - 12:01 AM
+package com.loolcorp.alipathdemo.config;
+
+import com.loolcorp.alipathdemo.security.UserDetailServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+/**
  * @author sskma
+ * @Created 02/06/2024 - 12:01 AM
  * @project alipathdemo
- * 
  */
+
+@Configuration
+@EnableMethodSecurity
 public class WebSecurityConfig {
+    @Autowired
+    UserDetailServiceImpl userDetailsService;
+
+    @Autowired
+    private AuthEntryPointJwt unauthorizedHandler;
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeRequests().antMatchers("/api/auth/**").permitAll()
+                .antMatchers("/api/test/**").permitAll()
+                .anyRequest().authenticated();
+
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
 }
