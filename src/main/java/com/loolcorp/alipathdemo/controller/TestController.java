d6f6b48680fb5115d3a8655371912ced9981e3f7
Sunimal Malkaklulage <sskmal@gmail.com>
2024-06-02 15:39:02 +0900

    Test and auth controllers

--- src/main/java/com/loolcorp/alipathdemo/controller/TestController.java
+++ src/main/java/com/loolcorp/alipathdemo/controller/TestController.java
@@ -1,8 +1,46 @@
-package com.loolcorp.alipathdemo.controller;/**
- * @Created 02/06/2024 - 2:53 AM
+package com.loolcorp.alipathdemo.controller;
+
+import com.loolcorp.alipathdemo.repository.RoleRepository;
+import com.loolcorp.alipathdemo.repository.UserRepository;
+import com.loolcorp.alipathdemo.security.JwtUtils;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+/**
  * @author sskma
+ * @Created 02/06/2024 - 2:53 AM
  * @project alipathdemo
- * 
  */
+@CrossOrigin (origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping ("/api/auth")
 public class TestController {
+    @GetMapping("/all")
+    public String allAccess() {
+        return "Public Content.";
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize ("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+    public String userAccess() {
+        return "User Content.";
+    }
+
+    @GetMapping("/mod")
+    @PreAuthorize("hasRole('MODERATOR')")
+    public String moderatorAccess() {
+        return "Moderator Board.";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String adminAccess() {
+        return "Admin Board.";
+    }
+
 }
