73139aa22e4e420e2da47595473299c24cae6880
Sunimal Malkaklulage <sskmal@gmail.com>
2024-06-03 01:18:37 +0900

    payload response check

--- src/main/java/com/loolcorp/alipathdemo/payload/response/JwtResponse.java
+++ src/main/java/com/loolcorp/alipathdemo/payload/response/JwtResponse.java
@@ -25,4 +25,12 @@ public class JwtResponse {
     private List <String> roles;
 
 
+    public JwtResponse(String accessToken, String id, String username, String email, List<String> roles) {
+        this.token = accessToken;
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.roles = roles;
+    }
+
 }
--- src/main/java/com/loolcorp/alipathdemo/payload/response/ResponseMessage.java
+++ src/main/java/com/loolcorp/alipathdemo/payload/response/ResponseMessage.java
@@ -1,8 +1,23 @@
-package com.loolcorp.alipathdemo.payload.response;/**
- * @Created 02/06/2024 - 3:57 PM
+package com.loolcorp.alipathdemo.payload.response;
+
+/**
  * @author sskma
+ * @Created 02/06/2024 - 3:57 PM
  * @project alipathdemo
- * 
  */
 public class ResponseMessage {
+
+    private String message;
+
+    public ResponseMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
